88295d2ccf85452b90323141c898e994eb70c4a5
Ashish <blr.ashu@gmail.com>
2018-03-15 19:49:04 +0000

    added Sign out method

--- app/src/main/java/nexumcorp/projectnexum/MainActivity.java
+++ app/src/main/java/nexumcorp/projectnexum/MainActivity.java
@@ -1,8 +1,11 @@
 package nexumcorp.projectnexum;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import nexumcorp.projectnexum.accounts.LoginActivity;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private FirebaseAuth.AuthStateListener mauthStateListener;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +51,7 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        setupFirebaseListener();
     }
 
     @Override
@@ -59,6 +71,7 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    //Logout System
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -68,11 +81,45 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            Log.d(TAG,"Attempting to sign out User");
+            FirebaseAuth.getInstance().signOut();
         }
 
         return super.onOptionsItemSelected(item);
     }
+    private void setupFirebaseListener(){
+        Log.d(TAG,"Setting up the auth state listener");
+        mauthStateListener= new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user= firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG,"signed_in"+user.getUid());
+                }else{
+                    Log.d(TAG,"onAuthstateChanged: signed_out");
+                    Toast.makeText(MainActivity.this,"Signed out",Toast.LENGTH_SHORT).show();
+                    Intent intent= new Intent(MainActivity.this, LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mauthStateListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mauthStateListener != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mauthStateListener);
+        }
+    }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
