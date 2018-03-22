d7b55281cfd731c97971b7856a5d0305739d7b81
Ashish <blr.ashu@gmail.com>
2018-03-22 22:15:01 +0000

    Added fav Btn

--- app/src/main/java/nexumcorp/projectnexum/add_business.java
+++ app/src/main/java/nexumcorp/projectnexum/add_business.java
@@ -3,7 +3,6 @@ package nexumcorp.projectnexum;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,18 +18,5 @@ public class add_business extends Fragment {
         View view =inflater.inflate(R.layout.add_business,container,false);
         return view;
     }
-    public void viewPost(String postid){
-        viewpost fragment = new viewpost();
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-
-        Bundle args= new Bundle();
-        args.putString(getString(R.string.arg_post_id),postid);
-        fragment.setArguments(args);
-
-        transaction.replace(R.id.container,fragment,getString(R.string.fragment_view_post));
-        transaction.addToBackStack(getString(R.string.fragment_view_post));
-        transaction.commit();
-
-        mFrameLayout.setVisibility(View.VISIBLE);
-    }
+    
 }
